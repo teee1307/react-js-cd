330fdd67b6aebdebb7fd0e75ca09d5b309f33cae
teee1307 <tanaya.s@crestinfosystems.com>
2024-04-12 14:56:32 +0530

    codedeploy.tf modified

--- infra/codedeploy.tf
+++ infra/codedeploy.tf
@@ -1,28 +1,26 @@
-# create a CodeDeploy application
+# Create a CodeDeploy application
 resource "aws_codedeploy_app" "main" {
   name = "Quizz_App"
 }
 
-# create a deployment group
+# Create a deployment group
 resource "aws_codedeploy_deployment_group" "main" {
-  app_name              = "${aws_codedeploy_app.main.name}"
+  app_name              = aws_codedeploy_app.main.name
   deployment_group_name = "Sample_DepGroup"
   service_role_arn      = data.aws_iam_role.existing_role.arn
 
   deployment_config_name = "CodeDeployDefault.OneAtATime" # AWS defined deployment config
 
-  ec2_tag_filter = {
-    key   = "Name"
-    type  = "KEY_AND_VALUE"
-    value = "CodeDeployDemo"
+  # Define EC2 tag filters to specify the instances to include in the deployment group
+  ec2_tag_filter {
+    key    = "Name"
+    type   = "KEY_AND_VALUE"
+    value  = "CodeDeployDemo"
   }
 
-  # trigger a rollback on deployment failure event
+  # Trigger a rollback on deployment failure event
   auto_rollback_configuration {
     enabled = true
-    events = [
-      "DEPLOYMENT_FAILURE",
-    ]
+    events  = ["DEPLOYMENT_FAILURE"]
   }
 }
-
